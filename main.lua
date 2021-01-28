84164ef87bb7b8d1df0f0c5f883844e25d974b78
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2021-01-28 15:04:19 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -30,9 +30,51 @@ getgenv().Notify = function(title, text, icon, time)
     })
 end
 
+
+getgenv().Noclip = function()
+local Players = game:GetService("Players")
+local RunS = game:GetService("RunService")
+local PLR, Mouse = Players.LocalPlayer, Players.LocalPlayer:GetMouse();
+local rawmet, Index, NIndex, NCall, Caller = getrawmetatable(game), getrawmetatable(game).__index, getrawmetatable(game).__newindex, getrawmetatable(game).__namecall, checkcaller or is_protosmasher_caller
+local noclip, KeyT = false, "x" 
+setreadonly(rawmet, false)
+
+RunS.Stepped:Connect(function()
+	if noclip == true and PLR and PLR.Character and PLR.Character:FindFirstChild("Humanoid") then
+		pcall(function()
+			PLR.Character.Head.CanCollide = false
+			PLR.Character.Torso.CanCollide = false
+		end)
+	end
+end)
+
+rawmet.__newindex = newcclosure(function(self, crv, Value)
+	if Caller() then
+		return NIndex(self, crv, Value)
+	end
+	if tostring(self) == "HumanoidRootPart" or tostring(self) == "Torso" then
+		if crv == "CFrame" and self:IsDescendantOf(PLR.Character) then
+			return true
+		end
+	end
+	return NIndex(self, crv, Value)
+end)
+setreadonly(rawmet, true)
+
+Mouse.KeyDown:Connect(function(Key)
+	if Key == KeyT then
+		noclip = not noclip
+		Notify("Entry-Null", "Noclip: "..tostring(noclip), "", 3)
+	end
+end)
+end
+
 Get = game
 Plrs = Get.Players
 Client = Get.Players.LocalPlayer
 ClientChar = Get.Players.LocalPlayer.Character
 Work = game.Workspace
 load = loadstring
+local RnS = game:GetService("RunService")
+local Mouse =  Players.LocalPlayer:GetMouse()
+local rawmet, Index, NIndex, NCall, Caller = getrawmetatable(game), getrawmetatable(game).__index, getrawmetatable(game).__newindex, getrawmetatable(game).__namecall, checkcaller or is_protosmasher_caller
