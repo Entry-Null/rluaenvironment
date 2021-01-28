5db3590bd654f03e00bcbd2ad894b64b545a8ae1
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2021-01-28 13:42:43 -0600

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,31 @@
+local function rm(path)
+path:Remove()
+end
+
+local function ChangeValue(path, valname, New)
+path[""..valname..""] = New
+end
+
+local function GetVal(path, valname)
+print(path[""..valname..""])
+local ret = path[""..valname..""]
+return ret
+end
+
+local function RemoteCall(path, args, args2)
+path:FireServer(args, args2)
+end
+
+local function CloneToPath(org, new)
+local orginal = org:Clone()
+orginal.Parent = new
+end
+
+Get = game
+Plrs = Get.Players
+Client = Get.Players.LocalPlayer
+ClientChar = Get.Players.LocalPlayer.Character
+Work = game.Workspace
+load = loadstring
+
+
