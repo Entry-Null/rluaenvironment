6939c9b01cc5b1cccd119dc61735fd3cc0b95616
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2021-01-30 05:17:26 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -71,6 +71,48 @@ end
 setreadonly(mt,true)
 end
 
+_G.objs = {}
+
+getgenv().SPOOF = function(obj, property, value)
+	local s = pcall(function()
+		local lol = obj[property]
+	end)
+	if s then
+		table.insert(_G.objs, {obj, property, value})
+	end
+end
+
+getgenv().findwithintable = function(o, p)
+	for i, v in pairs(_G.objs) do
+		if table.find(v, o) and table.find(v, p) then
+			return v
+		end
+	end
+	return nil
+end
+
+getgenv().DESPOOF = function(obj, property)
+	local lol = findwithintable(obj, property)
+	if lol then
+		table.remove(_G.objs, table.find(_G.objs, lol))
+	end
+end
+
+
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
+old = mt.__index
+
+mt.__index = newcclosure(function(o, p)
+	local t = findwithintable(o, p)
+	if t ~= nil then
+		return t[3]
+	end
+	
+	return old(o, p)
+end)
+
+
 getgenv().NOKICK = function()
 local mt = getrawmetatable(game)
 local oldnc = mt.__namecall
@@ -263,7 +305,10 @@ local f = loadstring(code);
  f()
 end
 
-
+while true do
+wait(4)
+print(FLOATGEN(341, 213888))
+end
 
 Get = game
 Plrs = Get.Players
