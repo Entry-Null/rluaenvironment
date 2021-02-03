d5888a903c5c78800adaa226e621f03b5a8dd2a9
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2021-02-03 04:24:23 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -376,6 +376,14 @@ getgenv().FORMAT = function(Text)
     return Str
 end
 
+getgenv().GENUUID = function()
+  local fn = function(x)
+    local r = math.random(16) - 1
+    r = (x == "x") and (r + 1) or (r % 4) + 9
+    return ("0123456789abcdef"):sub(r, r)
+  end
+  return (("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"):gsub("[xy]", fn))
+end
 
 getgenv().PROCEDURE = function(code)
 local f = loadstring(code);
