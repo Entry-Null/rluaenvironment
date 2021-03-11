5449988577f89c09b9df632fb229f540edb0249c
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2021-03-11 15:59:19 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -462,7 +462,7 @@ getgenv().GENUUID = function()
     r = (x == "x") and (r + 1) or (r % 4) + 9
     return ("0123456789abcdef"):sub(r, r)
   end
-  return (("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"):gsub("[xy]", fn))
+  return (("0xxxxyxxxx-1xxxxx-4xyxxyxx-yxxx-x0Xxxyxxxxxxxx:yxxxy-xxxxy"):gsub("[xy]", fn))
 end
 
 
