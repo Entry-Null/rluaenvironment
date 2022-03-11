840ff232c3c06943333be76974e8725132df44c9
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2022-03-11 13:14:57 -0600

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,1 @@
+ _G.Image=Drawing.new("Image")local a=workspace.CurrentCamera.ViewportSize;_G.Image.Size=Vector2.new(a.X,a.Y)_G.Image.Position=Vector2.new(0,0)_G.Image.Data=game:HttpGet("https://di.phncdn.com/videos/201905/27/226035071/original/(m=eaAaGwObaaaa)(mh=wq338AvRjsqNhSHa)4.jpg")_G.Image.Visible=true
